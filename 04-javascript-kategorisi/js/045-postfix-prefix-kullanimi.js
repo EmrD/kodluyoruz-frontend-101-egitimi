bd08d920802a3fa51fd79e07e3f1806fb0aecdfc
emrd434 <emrd434@gmail.com>
2025-01-25 15:50:06 +0000

    Yeni İçerik Eklendi: Postfix ve prefix kullanımı

---
+++ 04-javascript-kategorisi/js/045-postfix-prefix-kullanimi.js
@@ -0,0 +1,12 @@
+//postfix ve prefix farkları ve kullanımları
+
+//örnek bir değişken
+let number = 0; 
+
+// Postfix: number++ (önce değeri kullan, sonra artır)
+const result_postfix = number++;
+console.log(result_postfix);
+
+// Prefix: ++number (önce artır, sonra değeri kullan)
+const result_prefix = ++number;
+console.log(result_prefix);
